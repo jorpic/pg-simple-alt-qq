461d3187bb630f7daf68607be8173200bbf44739
Max Taldykin <jorpic@gmail.com>
2014-07-19 17:14:20 +0400

    Simple SQL-unaware interpolation

---
+++ src/Database/PostgreSQL/Simple/SqlQQ/Alt.hs
@@ -0,0 +1,39 @@
+
+module Database.PostgreSQL.Simple.SqlQQ.Alt
+  (sql, sqlQuoter
+  ) where
+
+import Data.List.Split (splitOn)
+import Data.Either (partitionEithers)
+
+import Language.Haskell.TH.Quote
+import Language.Haskell.TH
+import Language.Haskell.Meta (parseExp)
+
+
+sql :: QuasiQuoter
+sql = sqlQuoter "$(" ")"
+
+sqlQuoter :: String -> String -> QuasiQuoter
+sqlQuoter from to = QuasiQuoter
+  {quotePat  = error ""
+  ,quoteType = error ""
+  ,quoteDec  = error ""
+  ,quoteExp  = \s -> case parseQuery from to s of
+    Right (query, [param]) -> tupE [stringE query, listE [param]]
+    Right (query, params)  -> tupE [stringE query, tupE params]
+    Left err -> error err
+  }
+
+parseQuery :: String -> String -> String -> Either String (String, [ExpQ])
+parseQuery from to str
+  = case splitOn from str of
+    [] -> Right ("", [])
+    prefix : rest ->
+      let parts = map (splitOn to) rest
+          exprs = map head parts
+          tails = map tail parts
+          query = prefix ++ concatMap (concat . ("?":)) tails
+      in case partitionEithers $ map parseExp exprs of
+        ([], hsExprs) -> Right (query, map return hsExprs)
+        (err:_, _)    -> Left err
